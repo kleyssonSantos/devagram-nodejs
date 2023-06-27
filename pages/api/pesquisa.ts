adfa1a9df2850447d517fcabe89a57ac5bc78020
kleysson <129602412+kleyssonSantos@users.noreply.github.com>
2023-06-26 21:52:41 -0300

    pesquisa de usuarios

---
+++ pages/api/pesquisa.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {RespostaPadaoMsg} from '../../types/RespostaPadraoMsg';
+import { validarTokenJWT } from "@/middlewares/validarTokenJWT";
+import { conectarMongoDB } from "@/middlewares/conectarMongoDB";
+import { UsuarioModel } from "@/models/UsuarioModel";
+
+const pesquisaEndpoint = async (req : NextApiRequest, res : NextApiResponse <RespostaPadaoMsg | any[]>) => {
+
+    try{
+        if(req.method === 'GET'){
+            
+            const {filtro} = req.query;
+
+            if(!filtro || filtro.length < 2){
+                return res.status(400).json({erro : 'Favor informar pelo menos 2 caracteres para a busca'});
+            }
+
+            const usuariosEncontrados = await UsuarioModel.find({
+              $or: [{nome : {$regex : filtro, $options : 'i'}},
+                 { email : {$regex : filtro, $options : 'i'}}]
+            });
+
+            return res.status (200).json(usuariosEncontrados);
+        }
+        return res.status(405).json({erro: 'Metodo infomado mao e valido'})
+    }catch(e){
+        console.log(e);
+        return res.status(500).json({erro : 'Nao foi possivel buscar usuarios :' + e});
+    }
+}
+
+export default validarTokenJWT(conectarMongoDB(pesquisaEndpoint));
