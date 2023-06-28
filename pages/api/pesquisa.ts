c2fbc9bf336763196b3dcafe7f5236403e96a9b1
kleysson <129602412+kleyssonSantos@users.noreply.github.com>
2023-06-27 21:58:36 -0300

    pesquisa por nome e id

--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -8,25 +8,35 @@ const pesquisaEndpoint = async (req : NextApiRequest, res : NextApiResponse <Res
 
     try{
         if(req.method === 'GET'){
-            
-            const {filtro} = req.query;
-
-            if(!filtro || filtro.length < 2){
-                return res.status(400).json({erro : 'Favor informar pelo menos 2 caracteres para a busca'});
+            if(req?.query?.id){
+                const usuariosEncontrado = await UsuarioModel.findById(req?.query?.id);
+                if(!usuariosEncontrado){
+                    return res.status(400).json({erro : 'Usuario nao encontrado'});
+                }
+                usuariosEncontrado.senha = null;
+                return res.status(200).json(usuariosEncontrado);
+            }else{
+                const {filtro} = req.query;
+                if(!filtro || filtro.length < 2){
+                    return res.status(400).json({erro : 'Favor informar pelo menos 2 caracteres para a busca'});
+                }
+    
+                const usuariosEncontrados = await UsuarioModel.find({
+                  $or: [{nome : {$regex : filtro, $options : 'i'}},
+                     { email : {$regex : filtro, $options : 'i'}}]
+                });
+    
+                return res.status (200).json(usuariosEncontrados);
             }
+            
+         }
+         return res.status(405).json({erro: 'Metodo infomado mao e valido'})
+     }catch(e){
+         console.log(e);
+         return res.status(500).json({erro : 'Nao foi possivel buscar usuarios :' + e});
+     }
+ }
 
-            const usuariosEncontrados = await UsuarioModel.find({
-              $or: [{nome : {$regex : filtro, $options : 'i'}},
-                 { email : {$regex : filtro, $options : 'i'}}]
-            });
-
-            return res.status (200).json(usuariosEncontrados);
-        }
-        return res.status(405).json({erro: 'Metodo infomado mao e valido'})
-    }catch(e){
-        console.log(e);
-        return res.status(500).json({erro : 'Nao foi possivel buscar usuarios :' + e});
-    }
-}
-
-export default validarTokenJWT(conectarMongoDB(pesquisaEndpoint));
+ 
+ export default validarTokenJWT(conectarMongoDB(pesquisaEndpoint));
+            
